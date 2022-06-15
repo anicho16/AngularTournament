adae395c3f53ab302b303bbf54f5058e9ac88c53
Amber Nicholas <anicho16@my.westga.edu>
2022-06-14 21:06:37 -0400

    Added links to app.component.html and edited app-routing.module.ts to fix broken links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { BracketsComponent } from './tournament/brackets/brackets.component';
 import { RegistrationComponent } from './tournament/registration/registration.component';
+import { HelloComponent } from './tournament/hello/hello.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'hello', pathMatch: 'full' },
+  { path: '', redirectTo: '/hello', pathMatch: 'full' },
   {path: 'brackets', component: BracketsComponent},
   {path: 'registration', component: RegistrationComponent},
-  { path: '**', redirectTo: 'hello' }
+  {path: 'hello', component: HelloComponent},
+  { path: '**', redirectTo: '/hello' }
 ];
 
 @NgModule({
