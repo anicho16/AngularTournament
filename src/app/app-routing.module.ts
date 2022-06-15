9265ebaa1755e254fb8232ab86fe22d0674f4f18
Amber Nicholas <anicho16@my.westga.edu>
2022-06-14 20:28:14 -0400

    Added routing of brackets, registration, and hello to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BracketsComponent } from './tournament/brackets/brackets.component';
+import { RegistrationComponent } from './tournament/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'hello', pathMatch: 'full' },
+  {path: 'brackets', component: BracketsComponent},
+  {path: 'registration', component: RegistrationComponent},
+  { path: '**', redirectTo: 'hello' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
