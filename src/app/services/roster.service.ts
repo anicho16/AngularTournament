7f9b429d99da6f14112fba4bb7f64afb852c6b09
Amber Nicholas <anicho16@my.westga.edu>
2022-06-15 19:42:28 -0400

    Created Roster Service

---
+++ src/app/services/roster.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Registration } from 'app/tournament/registration';
+
+@Injectable()
+export class RosterService {
+
+  private contestants: Contestants[];
+  constructor() {
+    this.contestants = [
+    ];
+   }
+
+  getContestants() : Contestants[] {
+    return this.contestants;
+  }
+
+  addContestant(player: string) {
+    if(player == null || player = "") {
+      throw new Error("Player must contain a name.")
+    }
+
+    for (string contestant of this.contestants) {
+      if(player.localeCompare(contestant)) {
+        throw new Error("Player already exists.")
+      }
+
+      if (angular.equals(player, contestant)) {
+        throw new Error("Angular: Player already exits.")
+      }
+    }
+
+    this.contestants.push(player);
+  }
+}
