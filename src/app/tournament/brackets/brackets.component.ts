498f65e4c1bc0e3acb59be859b3876808b6df527
Amber Nicholas <anicho16@my.westga.edu>
2022-06-19 21:59:59 -0400

    Created logic for completeRound method of brackets component

--- src/app/tournament/brackets/brackets.component.ts
+++ src/app/tournament/brackets/brackets.component.ts
@@ -10,6 +10,7 @@ export class BracketsComponent {
   public contestants : string[];
   public roundNumber : number;
   public winners : string[];
+  public message : string;
 
   constructor(private rosterService: RosterService) {
     this.contestants = this.rosterService.getContestants();
@@ -18,15 +19,17 @@ export class BracketsComponent {
   }
 
   completeRound(matchForm) {
-    console.log(this.winners);
+    console.log("Number of winners is " + this.winners.length + "which is " + this.winners);
 
     if(this.winners.length == 1) {
-
+      this.message = "Winner: " + this.winners[0];
     } else if (this.winners.length == 2) {
       this.rosterService.resetContestants();
       this.rosterService.addContestant(this.winners[0]);
       this.rosterService.addContestant(this.winners[1]);
+      this.winners = [];
       this.contestants = this.rosterService.getContestants();
+      this.roundNumber++;
     } else {
       this.rosterService.resetContestants();
       this.rosterService.addContestant(this.winners[0]);
@@ -34,8 +37,7 @@ export class BracketsComponent {
       this.rosterService.addContestant(this.winners[2]);
       this.rosterService.addContestant(this.winners[3]);
       this.contestants = this.rosterService.getContestants();
+      this.roundNumber++;
     }
-        this.roundNumber++;
   }
-
 }
